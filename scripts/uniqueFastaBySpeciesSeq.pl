6869f23c2ed1ddf626dbbed2b531eb6f2dd7a489
Matthew Cannon <matthewcannon@hush.igs.umaryland.edu>
2019-08-19 15:14:26 -0400

    fixed uniqueFastaBySpecies.pl script and modified pipeline to get rid of the sequence trimming step since it isn't needed any longer with changes I made to bsPrimerTree.pl

--- scripts/uniqueFastaBySpeciesSeq.pl
+++ scripts/uniqueFastaBySpeciesSeq.pl
@@ -20,15 +20,12 @@ use Pod::Usage;
 
 my $verbose;
 my $help;
-my $taxa;
 my $fasta;
 
 # i = integer, s = string
 GetOptions ("verbose"           => \$verbose,
             "help"              => \$help,
-	    "taxonomy=s"        => \$taxa,
 	    "fasta=s"           => \$fasta
-
       )
  or pod2usage(0) && exit;
 
@@ -38,11 +35,6 @@ pod2usage(1) && exit if ($help);
 ##############################
 # Global variables
 ##############################
-my $taxaHeader;
-my @taxaCols;
-my $giCol;
-my $speciesCol;
-my %taxaHash;
 my %seqHash;
 
 ##############################
@@ -51,32 +43,8 @@ my %seqHash;
 
 
 ##############################
-### Pull in taxa file and make hash -> $hash{gi} = species
-### More stuff
-
-open TAXAFILE, "$taxa" or die "Could not open taxonomy input\nWell, crap\n";
-$taxaHeader = <TAXAFILE>;
-chomp $taxaHeader;
-@taxaCols = split "\t", $taxaHeader;
-
-for(my $i = 0; $i < scalar(@taxaCols); $i++) {
-    $giCol = $i if $taxaCols[$i] eq "gi";
-    $speciesCol = $i if $taxaCols[$i] eq "species";
-}
-
-while (my $input = <TAXAFILE>){
-    chomp $input;
-    my @cols = split "\t", $input;
-    if($cols[$speciesCol] !~ /sp\.|isol\.|isolate|cf\./) {
-	$taxaHash{$cols[$giCol]} = $cols[$speciesCol];
-    } 
-}
-
-
-
-##############################
 ### Pull in fasta file and keep one entry for each species:sequence combo
-### More stuff
+
 local $/ = "\n>";
 open FASTAFILE, "$fasta" or die "Could not open fasta input\nWell, crap\n";
 while (my $input = <FASTAFILE>){
@@ -86,12 +54,13 @@ while (my $input = <FASTAFILE>){
     my $seq = join("", @rawSequence);
     $seq =~ s/[\t\s]//g;
     $seq = uc($seq);
-    if(exists($taxaHash{$header})) {
-	my $species = $taxaHash{$header};
-	if(!exists($seqHash{$species . $seq})) {
-	    print ">", $header, "\n", $seq, "\n";
-	    $seqHash{$species . $seq} = 1;
-	}
+
+    my $species = $header;
+    $species =~ s/:sk-.+//; # cut off everything but the species
+    
+    if(!exists($seqHash{$species . $seq})) {
+	print ">", $header, "\n", $seq, "\n";
+	$seqHash{$species . $seq} = 1;	
     }
 }
 
