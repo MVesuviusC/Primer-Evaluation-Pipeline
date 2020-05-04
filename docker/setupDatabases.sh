04d11ee22e1231827def7ddc5609e92ff2b81940
MVesuviusC <matthewvc1@gmail.com>
2020-05-04 10:35:40 -0400

    Added a bunch of files so I can make a docker image to run this easily

---
+++ docker/setupDatabases.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# This script will download the NCBI blast databases needed as well as
+# create the taxonomy database required
+
+cd /usr/local/databases/blast/
+
+update_blastdb.pl --decompress taxdb
+
+update_blastdb.pl --decompress nt
+
+cd /home/runDir
+
+makeTaxonomyDb.pl -v --outDir /usr/local/databases/tax/ 
